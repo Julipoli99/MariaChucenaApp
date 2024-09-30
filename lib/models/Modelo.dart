e071874ba4525c14bad14594bc785b65b95a55b4
Julipoli99 <Polimenijulian9@gmail.com>
2024-09-30 19:15:52 -0300

    Agregado de paquete 'http' para hacer peticiones a la api, llamado get a los modelos

--- lib/models/Modelo.dart
+++ lib/models/Modelo.dart
@@ -1,18 +1,37 @@
 class Modelo {
-  String codigo;
-  String genero;
-  String prenda;
-  String tipo;
-  String tela;
+  final String codigo;
+  final String nombre;
+  final bool tieneTelaSecundaria;
+  final bool tieneTelaAuxiliar;
+  final String genero;
+  final List<dynamic> observaciones;
+  final List<dynamic> avios;
+  final List<dynamic> curva;
+  final String categoriaTipo;
 
   Modelo({
     required this.codigo,
+    required this.nombre,
+    required this.tieneTelaSecundaria,
+    required this.tieneTelaAuxiliar,
     required this.genero,
-    required this.prenda,
-    required this.tipo,
-    required this.tela,
+    required this.observaciones,
+    required this.avios,
+    required this.curva,
+    required this.categoriaTipo,
   });
 
-
-
+  factory Modelo.fromJson(Map<String, dynamic> json) {
+    return Modelo(
+      codigo: json['codigo'],
+      nombre: json['nombre'],
+      tieneTelaSecundaria: json['tieneTelaSecundaria'],
+      tieneTelaAuxiliar: json['tieneTelaAuxiliar'],
+      genero: json['genero'],
+      observaciones: json['observaciones'] ?? [],
+      avios: json['avios'] ?? [],
+      curva: json['curva'] ?? [],
+      categoriaTipo: json['categoria']['tipo'],
+    );
+  }
 }
--- lib/pages/Crud/modeloCrudView.dart
+++ lib/pages/Crud/modeloCrudView.dart
@@ -1,30 +1,48 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:gestion_indumentaria/models/Avios.dart';
 import 'package:gestion_indumentaria/models/Modelo.dart';
 import 'package:gestion_indumentaria/widgets/tablaCrud/TablaCrud.dart';
+import 'package:http/http.dart' as http;
 
-class ModelCrudView extends StatelessWidget {
+class ModelCrudView extends StatefulWidget {
   ModelCrudView({super.key});
 
+  @override
+  State<ModelCrudView> createState() => _ModelCrudViewState();
+}
+
+class _ModelCrudViewState extends State<ModelCrudView> {
+  
+  List<dynamic> modelos = [];
+
   // Ejemplo para el crud de Modelos
-  final List<Modelo> models = [
-    Modelo(codigo: "M001", genero: "Masculino", prenda: "Remera", tipo: "Hombre", tela: "Secundaria"),
-    Modelo(codigo: "M002", genero: "Femenino", prenda: "Pantalon", tipo: "Mujer", tela: "Primaria"),
-    Modelo(codigo: "M003", genero: "Masculino", prenda: "Camisa", tipo: "Hombre", tela: "Primaria"),
+   List<Modelo> models = [
+    
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    // Llamamos al fetch cuando la página se carga
+    fetchModels();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: TablaCrud<Modelo>(
         tituloAppBar: 'Modelos registrados', // Titulo del appBar
-        encabezados: const ["CODIGO", "GENERO", "PRENDA", "TIPO", "TELA", "OPCIONES"], // Encabezados
-        items: models,   // Lista de usuarios
+        encabezados: const ["CODIGO", "NOMBRE", "GENERO", "TIPO", "OPCIONES"], // Encabezados
+        items: models,   // Lista de modelos
         dataMapper: [ // Celdas/valores
           (model) => Text(model.codigo.toString()),
+          (model) => Text(model.nombre),
           (model) => Text(model.genero),
-          (model) => Text(model.prenda),
-          (model) => Text(model.tipo),
-          (model) => Text(model.tela),
+          (model) => Text(model.categoriaTipo),
+          //(model) => Text(model.tipo),
+          //(model) => Text(model.tela),
           //Parte de Opciones, se le pasa una funcion que retorna una List de Widgets en este caso Row.
           (model) => Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -53,4 +71,37 @@ class ModelCrudView extends StatelessWidget {
       ),
     );
   }
+
+  void fetchModels() async {
+  const url = "https://maria-chucena-api-production.up.railway.app/modelo";
+  final uri = Uri.parse(url);
+  final response = await http.get(uri);
+  final List<dynamic> jsonData = jsonDecode(response.body);
+
+  setState(() {
+    models = jsonData.map((json) {
+      List<Avios> avios = (json['avios'] as List).map((av) {
+        return Avios(
+          id: av['avio']['id'].toString(),
+          nombre: av['avio']['nombre'],
+          proveedores: av['avio']['codigoProveedor'], // Puedes ajustar según la estructura de tu clase Avios
+        );
+      }).toList();
+
+      return Modelo(
+        codigo: json['codigo'],
+        genero: json['genero'], // Ajustando para usar el tipo de la categoría como prenda
+        nombre: json['nombre'],
+        tieneTelaSecundaria: json['tieneTelaSecundaria'],
+        tieneTelaAuxiliar: json['tieneTelaAuxiliar'],
+        avios: avios,
+        curva: json['curva'],
+        categoriaTipo: json['categoria']['tipo'],
+        observaciones: json['observaciones']
+      );
+    }).toList();
+  });
+
+  print("Modelos cargados");
+}
 }
