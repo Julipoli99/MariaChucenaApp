a7244062b214b4914fbf6e89362d415e97c74741
lautaro <116090828+lautaro2034@users.noreply.github.com>
2024-10-28 15:00:52 -0300

    clase tipo producto

--- lib/models/TipoProducto.dart
+++ lib/models/TipoProducto.dart
@@ -1,8 +1,14 @@
+// ignore: constant_identifier_names
+enum TipoEnum { AVIO, TELA }
+
+// ignore: constant_identifier_names
+enum UnidadMetricaEnum { UNIDAD, METRO, KILO }
+
 class TipoProducto {
   int? id; // El ID se auto genera
   String nombre;
-  String tipo;
-  String unidadMetrica;
+  TipoEnum tipo;
+  UnidadMetricaEnum unidadMetrica;
 
   TipoProducto({
     this.id,
@@ -14,20 +20,22 @@ class TipoProducto {
   // Método para convertir de JSON a TipoProducto
   factory TipoProducto.fromJson(Map<String, dynamic> json) {
     return TipoProducto(
-      id: json['id'] as int?, // El ID se puede recibir como null
+      id: json['id'] as int?,
       nombre: json['nombre'] as String,
-      tipo: json['tipo'] as String,
-      unidadMetrica: json['unidadMetrica'] as String,
+      tipo: TipoEnum.values
+          .firstWhere((e) => e.toString().split('.').last == json['tipo']),
+      unidadMetrica: UnidadMetricaEnum.values.firstWhere(
+          (e) => e.toString().split('.').last == json['unidadMetrica']),
     );
   }
 
   // Método para convertir de TipoProducto a JSON
   Map<String, dynamic> toJson() {
     return {
-      'id': id, // Solo enviar el ID si no es null
+      'id': id,
       'nombre': nombre,
-      'tipo': tipo,
-      'unidadMetrica': unidadMetrica,
+      'tipo': tipo.toString().split('.').last,
+      'unidadMetrica': unidadMetrica.toString().split('.').last,
     };
   }
 }
