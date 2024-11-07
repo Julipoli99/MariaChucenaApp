21a3b22452b2b91daf175a6563e250a5feb4cd1e
Victoria Purcaro <mv.purcaro@gmail.com>
2024-11-06 21:01:27 -0300

    crear orden funciona :D

--- lib/models/talle.dart
+++ lib/models/talle.dart
@@ -7,11 +7,6 @@ class Talle {
     required this.nombre,
   });
 
-  @override
-  String toString() {
-    return nombre; // Devuelve solo el nombre del talle
-  }
-
   /// **Fábrica para crear un Avios desde JSON**
   factory Talle.fromJson(Map<String, dynamic> json) {
     return Talle(
