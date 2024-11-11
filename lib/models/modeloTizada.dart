69abb8add2b4b18cb71200d3b1c01f71c71c9b81
Victoria Purcaro <mv.purcaro@gmail.com>
2024-11-11 13:53:05 -0300

    cambios en tizada modelo entidad

--- lib/models/modeloTizada.dart
+++ lib/models/modeloTizada.dart
@@ -1,25 +1,30 @@
+import 'package:gestion_indumentaria/models/ModeloCorte.dart';
 import 'package:gestion_indumentaria/models/talleRepetecion.dart';
 
 class ModeloTizada {
   double consumo;
   int modeloCorteId;
   List<TalleRepeticion> curva;
+  ModeloCorte? modelo;
+  int? totalPrendas;
 
   ModeloTizada({
     required this.consumo,
     required this.modeloCorteId,
     required this.curva,
+    this.modelo,
+    this.totalPrendas,
   });
 
   factory ModeloTizada.fromJson(Map<String, dynamic> json) {
     return ModeloTizada(
-      consumo: json['consumo'] ?? 'Sin consumo definido',
-      modeloCorteId: json['modeloCorteId'] ?? 'Sin largo definido',
-      curva: (json['curva'] as List).map((tr) {
-        return TalleRepeticion.fromJson(
-            tr); // Asegúrate de que tu clase Avios tenga este método
-      }).toList(),
-    );
+        consumo: json['consumo'] ?? 'Sin consumo definido',
+        modeloCorteId: json['modeloCorteId'] ?? 'Sin largo definido',
+        curva: (json['curva'] as List).map((tr) {
+          return TalleRepeticion.fromJson(tr);
+        }).toList(),
+        modelo: ModeloCorte.fromJson(json['modelo']),
+        totalPrendas: json['totalPrendas']);
   }
   Map<String, dynamic> toJson() {
     return {
